37e3ce71c4063b74a3e50f788a771dec3522dcda
lucia <llcsaturnaliam@gmail.com>
2024-04-28 17:49:21 -0500

    :3 rust

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,13 @@ use std::{env, ffi::{c_void, CString}};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        panic!("usage: dll-inject <pid> <dll path>");
+    }
+
     let pid: u32 = args[1].parse().expect("error getting pid!");
     let dll_path: &str = &args[2];
 
-    println!("{}", dll_path);
     unsafe {
         let process: HANDLE = OpenProcess(PROCESS_ALL_ACCESS, true, pid).expect("error while getting the process handle!");
         let base_address: *mut c_void = VirtualAllocEx(process, None, dll_path.len() + 1, MEM_COMMIT, PAGE_READWRITE);
@@ -17,8 +20,9 @@ fn main() {
         };
 
         match WriteProcessMemory(process, base_address, dll_path_c, dll_path.len() + 1, None) {
-            Ok(_) => println!("wrote memory successfully!"),
+            Ok(_) => (),
             Err(_) => panic!("error while writing process memory!"),
         }
-    }
+
+   }
 }
